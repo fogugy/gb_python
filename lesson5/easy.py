cb92eb6f69e6b96a6ba3fbd0e20e261ba9022107
fogugy <fogugy@gmail.com>
2018-10-21 15:42:28 +0400

    lesson 5/ easy t3

--- lesson5/easy.py
+++ lesson5/easy.py
@@ -3,7 +3,6 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
-import path
 import os
 
 dir_names = [f'dir{x}' for x in range(1, 9)]
@@ -12,10 +11,18 @@ dir_names = [f'dir{x}' for x in range(1, 9)]
 def mk_dirs():
     for dir_name in dir_names:
         if not os.path.exists(dir_name):
-            os.makedirs(dir_name)
+            os.mkdir(dir_name)
 
 
 def rm_dirs():
     for dir_name in dir_names:
         if os.path.exists(dir_name):
-            os.removedirs(dir_name)
+            os.rmdir(dir_name)
+
+
+# Задача-2:
+# Напишите скрипт, отображающий папки текущей директории.
+
+def ls():
+    cwd = os.getcwd()
+    print([name for name in os.listdir(cwd) if os.path.isdir(os.path.join(cwd, name))])
