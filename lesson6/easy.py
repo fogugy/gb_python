44553a50b78c13915885cdd599f5c460f77ad47d
fogugy <fogugy@gmail.com>
2018-10-27 17:35:56 +0400

    lesson 6/ easy t2

--- lesson6/easy.py
+++ lesson6/easy.py
@@ -4,18 +4,22 @@
 import math
 
 
+def _get_range(p1, p2):
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+
 class Triangle:
     def __init__(self, *args):
         self._A = args[0]
         self._B = args[1]
         self._C = args[2]
 
-        self.set_props()
+        self._set_props()
 
-    def set_props(self):
-        self.AB = Triangle._get_range(self._A, self._B)
-        self.BC = Triangle._get_range(self._B, self._C)
-        self.AC = Triangle._get_range(self._A, self._C)
+    def _set_props(self):
+        self.AB = _get_range(self._A, self._B)
+        self.BC = _get_range(self._B, self._C)
+        self.AC = _get_range(self._A, self._C)
         self._p = self.AB + self.BC + self.AC
         self._h_ab = self._get_height(self.AB)
         self._h_bc = self._get_height(self.BC)
@@ -29,7 +33,7 @@ class Triangle:
     @A.setter
     def A(self, value):
         self._A = value
-        self.set_props()
+        self._set_props()
 
     @property
     def B(self):
@@ -38,7 +42,7 @@ class Triangle:
     @B.setter
     def B(self, value):
         self._B = value
-        self.set_props()
+        self._set_props()
 
     @property
     def C(self):
@@ -47,7 +51,7 @@ class Triangle:
     @C.setter
     def C(self, value):
         self._C = value
-        self.set_props()
+        self._set_props()
 
     @property
     def perimeter(self):
@@ -65,10 +69,51 @@ class Triangle:
             'AC': self._h_ac,
         }
 
-    @staticmethod
-    def _get_range(p1, p2):
-        return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
-
     def _get_height(self, side):
         hp = self._p / 2
         return round(2 * math.sqrt(hp * (hp - self.AB) * (hp - self.BC) * (hp - self.AC)) / side, 9)
+
+
+# Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
+# Предусмотреть в классе методы:
+# проверка, является ли фигура равнобочной трапецией;
+# вычисления: длины сторон, периметр, площадь.
+
+
+class Trapezium:
+    def __init__(self, *args):
+        self._points = {k: v for (k, v) in enumerate(args)}
+
+        self._set_props()
+
+    def _set_props(self):
+        pts = self.points
+        pts_cnt = len(pts)
+        self._sides = [_get_range(pts.get(i), pts.get(i + 1) if i < pts_cnt - 1 else pts.get(0)) for i in range(pts_cnt)]
+        self._p = sum(self.sides)
+        self._h = self.points.get(1)[1] - self.points.get(0)[1]
+        self._area = (self.sides[1] + self.sides[3]) * self.height / 2
+
+    @property
+    def points(self):
+        return self._points
+
+    @property
+    def sides(self):
+        return self._sides
+
+    @property
+    def perimeter(self):
+        return self._p
+
+    @property
+    def area(self):
+        return self._area
+
+    @property
+    def height(self):
+        return self._h
+
+    @property
+    def is_isosceles(self):
+        return self.points[1][0] - self.points[0][0] == self.points[3][0] - self.points[2][0]
