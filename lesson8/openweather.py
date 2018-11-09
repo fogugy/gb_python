081ab35cf85d549f320a7b985e93c05a4c09f5db
Yuri Zinovev <fogugy@gmail.com>
2018-11-09 20:31:10 +0400

    lesson 8/ openweather class mock

---
+++ lesson8/openweather.py
@@ -0,0 +1,12 @@
+CITIES_API = 'http://bulk.openweathermap.org/sample/city.list.json.gz'
+
+
+class OpenWeatherFacade:
+    def __init__(self):
+        pass
+
+    def fetch_cities(self):
+        pass
+
+    def save_cities(self):
+        pass
